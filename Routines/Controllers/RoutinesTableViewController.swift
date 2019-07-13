19b477735dfb37472c1649098cf6c3542ac0ee39
aic <aicprog@gmail.com>
2019-07-12 22:11:27 -0700

    Update UI for custom RoutineTableViewCell

--- Routines/Controllers/RoutinesTableViewController.swift
+++ Routines/Controllers/RoutinesTableViewController.swift
@@ -15,7 +15,7 @@ class RoutinesTableViewController: UITableViewController {
     
     let realm = try! Realm()
     var routines: Results<Routine>?
-    
+   // let cellSpacingHeight: CGFloat = 100
     //MARK: - IBoutlets
     
 
@@ -42,6 +42,7 @@ class RoutinesTableViewController: UITableViewController {
         let imageView = UIImageView(image: backgroundImage)
         imageView.contentMode = .scaleAspectFill
         self.tableView.backgroundView = imageView
+        
     }
     
     // MARK: - Table view data source
@@ -53,15 +54,39 @@ class RoutinesTableViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "routineCell", for: indexPath) as! RoutineTableViewCell
+        
+        
         
         if let routine = routines?[indexPath.row]{
             cell.routineName.text = routine.name
         }
         
+        
+        //test
+        cell.contentView.backgroundColor = UIColor.clear
+        
+        
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: "goToSubRoutine", sender: self)
+    }
+    
+    //MARK: - Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! SubRoutineTableViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow{
+            destinationVC.selectedRoutine = routines?[indexPath.row]
+            
+        }
+        
+    }
+
     //MARK: - IBActions
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -71,9 +96,18 @@ class RoutinesTableViewController: UITableViewController {
         //add New Alert Action
         let alertAction = UIAlertAction(title: "Add", style: .default) { (alert) in
             //create new Item
-            let newRoutine = Routine()
-            newRoutine.name = txtField?.text ?? "New Routine"
-            self.save(add: newRoutine)
+             
+            if let name = txtField?.text{
+                if !name.trimmingCharacters(in: .whitespaces).isEmpty{
+                    let newRoutine = Routine()
+                    newRoutine.name = txtField?.text ?? "New Routine"
+                    self.save(add: newRoutine)
+                    
+                    
+                }
+            }
+            
+           
         }
         //add textField
         alertController.addTextField { (alertTextField) in
@@ -106,49 +140,5 @@ class RoutinesTableViewController: UITableViewController {
         
     }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
